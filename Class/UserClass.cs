ae335e403746f5ca5270e318ec2f510a3ea4052a
mtoku <mtoku@DESKTOP-PT0KF0R>
2024-12-20 03:39:29 +0300

    .

---
+++ Class/UserClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_Management.Class
+{
+    internal class UserClass
+    {
+       /* if (UserClass.UserLevel == 1 || UserClass.UserLevel == 2 || 
+            UserClass.UserLevel == 3 || UserClass.UserLevel == 4)
+       */
+    }
+}
